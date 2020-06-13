287222b599bdd246bca227710b7c66de37e06a05
Jonathan Branski <32647040+JBranski@users.noreply.github.com>
2020-06-13 16:48:11 -0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,92 @@
+alert("hello");
+
+window.addEventListener('DOMContentLoaded', (e) =>{
+	// hide/show navigation on scroll
+	let lastScrollpos = window.pageYOffset;
+	let menuButtonCheck = document.querySelector(".menu-btn");
+	window.onscroll = function() {
+		let currentScrollPos = window.pageYOffset;
+		if (lastScrollpos > currentScrollPos) {
+			document.getElementById("top-nav").style.top = "0";
+		} else {
+			document.getElementById("top-nav").style.top = "-75px";
+		}
+		lastScrollpos = currentScrollPos;
+		menuButtonCheck.checked = false;
+	} 
+	
+	let subjectSelect = document.querySelector("select");
+	let defaultSelect = document.querySelector("select>option");
+	subjectSelect.addEventListener("change", function(){
+		subjectSelect.classList.remove("disabled");
+		defaultSelect.disabled = true;
+	});
+
+	
+
+});
+
+// Redirect to home page	
+if(window.location.href == "https://jonathanbranski.com/thankyou"){
+	setTimeout(
+		function(){
+			window.location.replace("https://jonathanbranski.com");
+		}, 3000
+	);
+}
+
+// FORM VALIDATION - CLEAN UP
+
+let contact = document.querySelector("form");
+let nameError = document.getElementById("nameError");
+let emailError = document.getElementById("emailError");
+let messageError = document.getElementById("messageError");
+
+let nameRegex = /[a-zA-Z][a-zA-Z ]+/;
+let emailRegex = /^(([^<>()[\]\\.,;:\s@\"]+(\.[^<>()[\]\\.,;:\s@\"]+)*)|(\".+\"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/;
+let urlRegex = /^(https?:\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/;
+
+let nameCheck = false;
+let emailCheck = false; 
+let messageCheck = false;
+
+contact.addEventListener("submit", function(e){
+	e.preventDefault();
+
+	let formName = document.getElementById("formName").value;
+	let formEmail = document.getElementById("formEmail").value;
+	let formMessage = document.getElementById("formMessage").value;
+
+	nameError.textContent = "";
+	emailError.textContent = "";
+	messageError.textContent = "";
+
+	nameCheck = false;
+	emailCheck = false;
+	messageCheck = false;
+	
+	if(formName == ""){
+		nameError.textContent = "Name field cannot be empty!";
+	} else if(!nameRegex.test(formName)){
+		nameError.textContent = "Name must be between 2-30 characters long!"
+	} else {nameCheck = true;}
+
+	if(formEmail == ""){
+		emailError.textContent = "Email field cannot be empty!";
+	} else if(!emailRegex.test(formEmail)){
+		emailError.textContent = "Please enter a valid email!";
+	} else {emailCheck = true;}
+
+	if(formMessage == ""){
+		messageError.textContent = "Mesage field cannot be empty!";
+	} else if (urlRegex.test(formMessage)){
+		messageError.textContent = "Please remove any links from your message!";
+	} else {messageCheck = true;}
+
+	if(nameCheck == true && emailCheck == true && messageCheck == true){
+		contact.action = "https://formspree.io/mnqbdqrp";
+		contact.submit;
+		window.location.replace("https://jonathanbranski.com/thankyou");
+		return false;
+	}
+});
---
+++ thankyou.php
@@ -0,0 +1,96 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<meta http-equiv="X-UA-Compatible" content="ie=edge">
+
+	<!-- Sets theme color for certain browsers -->
+	<meta name="theme-color" content="#9fg7e8">
+	<meta name="color-scheme" content="light dark">
+
+	<!-- 150 char max, may be pulled during search engine results -->
+	<meta name="description" content="Hello, I'm Jonathan and I will be your front-end developer today.">
+
+	<!-- Controls behavior of search engine crawling and indexing -->
+	<meta name="robots" content="index,follow">
+	<meta name="googlebot" content="index,follow">
+
+	<!-- Website owner verification -->
+	<meta name="google-site-verification" content="XwLetKl79qvxX1kMfiHSDeCU8lfDL-BEss0U_BqVq1s" />
+	<meta name="p:domain_verify" content="d81c260832648260ffaf073825f21c0e"/>
+
+	<!-- Facebook -->
+	<meta property="og:url"                 content="https://jonathanbranski.com/thankyou.php" />
+	<meta property="og:title"               content="Thank you! | Jonathan Branski" />
+	<meta property="og:description"         content="Hello, my name is Jonathan and this is my portfolio site. Please enjoy your stay and let me know if I can assist you." />
+	<meta property="og:site_name"           content="Jonathan Branski | Front-end Developer" />
+	<meta property="og:image"               content="jbranski-logo-black.png" />
+	<meta property="og:type"                content="website" />
+	<meta property="og:locale"              content="en_US" />
+
+	<!-- Twitter -->
+	<meta name="twitter:card"               content="Front-end developer portfolio website for Jonathan Branski.">
+	<meta name="twitter:site"               content="@jbranski96">
+
+	<!-- Favicon -->
+	<link rel="apple-touch-icon" sizes="180x180" href="./img/apple-touch-icon.png">
+	<link rel="icon" type="image/png" sizes="32x32" href="./img/favicon-32x32.png">
+	<link rel="icon" type="image/png" sizes="16x16" href="./img/favicon-16x16.png">
+	<link rel="manifest" href="./img/site.webmanifest">
+
+
+	<title>Thank You | Jonathan Branski</title>
+
+	<link href="https://fonts.googleapis.com/css?family=Montserrat|Open+Sans&display=swap" rel="stylesheet"> 
+	<link rel="stylesheet" href="./style.min.css">
+
+	 <!--[if lt IE 9]> 
+    <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/html5shiv/3.7.3/html5shiv.min.js"></script> 
+	<![endif]-->
+	
+	<!-- [if IE]>
+      <style type="text/css">
+
+        ..header-bg-circle, .contact-bg-circle {
+          filter:progid:DXImageTransform.Microsoft.AlphaImageLoader(src='sample_600x600.png', sizingMethod='scale');
+          background:none !important;
+        }
+
+        </style>
+    <![endif] -->
+	<title></title>
+</head>
+<body>
+	<nav class="top-nav" id="top-nav">
+		<a href="" class="logo"><img src="./img/logo-horizontal-black.svg" alt="Jonathan Branski | Front-end Developer black logo"></a>
+		<input class="menu-btn" type="checkbox" id="menu-btn" />
+		<label class="menu-icon" for="menu-btn"><span class="navicon"></span></label>
+		<ul class="menu">
+			<li><a href="index.php">Home</a></li>
+			<li><a href="index.php#about">About</a></li>
+			<li><a href="index.php#projects">Projects</a></li>
+			<li><a href="index.php#contact">Contact</a></li>
+		</ul>
+	</nav>
+	<header id="home">	
+		<div class="title">
+			<img src="./img/jbranski-logo-white.png" alt="Jonathan Branski white logo without text">
+			<h1>Thank you for your message!</h1>
+			<p class="redirect">You will be redirected shortly</p>
+		</div>
+		<div class="header-bg-circle">
+			<span></span>
+			<span></span>
+			<span></span>
+			<span></span>
+		</div>
+	</header>
+	<footer>
+		<p>
+			<!-- <a href="https://www.siteground.com/index.htm?afcode=5bee33cc7c6084149c4507ff237a39a1" target="_blank">hosted with SiteGround</a> -->
+		</p>
+	</footer>
+	<script src="scripts.js" type="text/javascript"></script>
+</body>
+</html>
